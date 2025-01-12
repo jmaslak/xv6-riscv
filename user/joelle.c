f8f2262385f9a2fd3b0530d0184cdd4d8d0bbc94
Joelle Maslak <jmaslak@antelope.net>
2025-01-12 13:03:58 -0700

    Print time

--- user/joelle.c
+++ user/joelle.c
@@ -1,10 +1,11 @@
 #include "kernel/types.h"
-// #include "kernel/fcntl.h"
+#include "kernel/syscall.h"
 #include "user/user.h"
 
 int
 main(int argc, char *argv[])
 {
+    printf("Time: %ld\n", time());
     printf("Hello, World. %20s foo\n", "a");
     printf("Hello, World. %-20s foo\n", "a");
     exit(0);
